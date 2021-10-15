aabf1041483c95c4ad6294610570167413efda15
Christoffer Wrigmar <christoffer.wrigmar@knowit.se>
2021-10-15 11:31:29 +0200

    Models folder and WC class

---
+++ KissKartan/KissKartan.App/Models/WC.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KissKartan.App.Models
+{
+    class WC
+    {
+    public int Id { get; set; }
+
+    }
+}
