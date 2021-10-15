4983553bde9395a51a31fe28f7ba1ab5184f877f
Christoffer Wrigmar <christoffer.wrigmar@knowit.se>
2021-10-15 11:23:32 +0200

    Refaktorering och nytt projekt för logik

---
+++ KissKartan/KissKartan.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KissKartan.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
